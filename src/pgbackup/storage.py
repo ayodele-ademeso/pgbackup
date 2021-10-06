bd5978867d75cda55858d5e01043c6edccd2871d
ayodele-ademeso <ademesoayodele@gmail.com>
2021-10-06 10:14:45 +0100

    Added the s3 function to storage module

--- src/pgbackup/storage.py
+++ src/pgbackup/storage.py
@@ -2,3 +2,6 @@ def local(outfile, infile):
     outfile.write(infile.read())
     outfile.close()
     infile.close()
+
+def s3(client, infile, bucket, name):
+    client.upload_fileobj(infile, bucket, name)
